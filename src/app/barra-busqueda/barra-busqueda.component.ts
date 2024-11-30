6ad5fd5e4b897c1946a4f64f7b8a44d754502ad5
oscfdezdz <oscfdezdz@uma.es>
2024-11-30 13:03:46 +0100

    Añadir componente básico de barra de búsqueda

---
+++ src/app/barra-busqueda/barra-busqueda.component.ts
@@ -0,0 +1,19 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-barra-busqueda',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './barra-busqueda.component.html',
+  styleUrl: './barra-busqueda.component.scss'
+})
+export class BarraBusquedaComponent {
+  textoBusqueda: string = '';
+
+  @Output() busqueda = new EventEmitter<string>();
+
+  onSearchChange() {
+    this.busqueda.emit(this.textoBusqueda);
+  }
+}
--- src/app/wikis/wikis.component.ts
+++ src/app/wikis/wikis.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WikisService } from './wikis.service';
+import { BarraBusquedaComponent } from '../barra-busqueda/barra-busqueda.component';
 
 @Component({
   selector: 'app-wikis',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BarraBusquedaComponent],
   templateUrl: './wikis.component.html',
   styleUrls: ['./wikis.component.scss']
 })
 export class WikisComponent implements OnInit {
   wikis: any[] = [];
+  wikisFiltradas: any[] = [];
+  textoBusqueda: string = '';
 
   constructor(private wikisService: WikisService) {}
 
@@ -18,10 +21,18 @@ export class WikisComponent implements OnInit {
     this.wikisService.getWikis().subscribe({
         next: (data) => {
             this.wikis = data;
+            this.wikisFiltradas = this.wikis;
         },
         error: (err) => {
             console.error('Error al obtener las wikis:', err);
         },
     });
+  }
+
+  onSearchTextChanged(textoBusqueda: string): void {
+    this.textoBusqueda = textoBusqueda;
+    this.wikisFiltradas = this.wikis.filter(wiki =>
+      wiki.nombre.toLowerCase().includes(textoBusqueda.toLowerCase())
+    );
   }
 }
