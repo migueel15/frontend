30f8a368faf6d54eb1eecc3b702cad0c2e705b57
migueel15 <miguelangeldorado10@gmail.com>
2024-12-01 17:42:39 +0100

    cambiado port

--- src/app/wikis/wikis.component.ts
+++ src/app/wikis/wikis.component.ts
@@ -1,49 +1,54 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { WikisService } from './wikis.service';
-import { Router } from '@angular/router';
-import { BarraBusquedaComponent } from '../barra-busqueda/barra-busqueda.component';
+import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { WikisService } from "./wikis.service";
+import { Router } from "@angular/router";
+import { BarraBusquedaComponent } from "../barra-busqueda/barra-busqueda.component";
 
 @Component({
-  selector: 'app-wikis',
+  selector: "app-wikis",
   standalone: true,
   imports: [CommonModule, BarraBusquedaComponent],
-  templateUrl: './wikis.component.html',
-  styleUrls: ['./wikis.component.scss']
+  templateUrl: "./wikis.component.html",
+  styleUrls: ["./wikis.component.scss"],
 })
 export class WikisComponent implements OnInit {
   wikis: any[] = [];
   wikisFiltradas: any[] = [];
-  textoBusqueda: string = '';
+  textoBusqueda: string = "";
 
-  constructor(private wikisService: WikisService, private router: Router) {}
+  constructor(
+    private wikisService: WikisService,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
     this.wikisService.getWikis().subscribe({
-        next: (data) => {
-            
-            this.wikis = data;
-            this.wikisFiltradas = this.wikis;
-        },
-        error: (err) => {
-            console.error('Error al obtener las wikis:', err);
-        },
+      next: (data) => {
+        this.wikis = data;
+        this.wikisFiltradas = this.wikis;
+      },
+      error: (err) => {
+        console.error("Error al obtener las wikis:", err);
+      },
     });
   }
 
   crearWiki() {
-    console.log('Crear nueva wiki');
+    console.log("Crear nueva wiki");
 
-    this.router.navigate(['/new_wiki']);
-    
+    this.router.navigate(["/new_wiki"]);
   }
 
   onSearchTextChanged(textoBusqueda: string): void {
     this.textoBusqueda = textoBusqueda;
-    this.wikisFiltradas = this.wikis.filter(wiki =>
+    this.wikisFiltradas = this.wikis.filter((wiki) =>
       wiki.nombre.toLowerCase().includes(textoBusqueda.toLowerCase())
     );
   }
 
-  
+  handleWikiClick(wikiId: any) {
+    console.log("Click en wiki:", wikiId);
+    this.router.navigate(["/wiki/", wikiId]);
+  }
 }
+
--- src/app/wikis/wikis.service.ts
+++ src/app/wikis/wikis.service.ts
@@ -1,19 +1,19 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class WikisService {
-  private apiUrl = 'http://localhost:8001/v2/wikis/';
+  private apiUrl = "http://localhost:8000/wikis/";
 
   constructor(private http: HttpClient) {}
 
   getWikis(): Observable<any[]> {
-    return this.http.get<{ wikis: any[] }>(this.apiUrl).pipe(
-        map((response) => response.wikis)
-    );
+    return this.http
+      .get<{ wikis: any[] }>(this.apiUrl)
+      .pipe(map((response) => response.wikis));
   }
 }
