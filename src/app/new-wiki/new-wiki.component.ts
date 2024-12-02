8b33d736c807b9a0c15cfc15c7b5d20f30fd45be
scrpti <pedroscarpati@uma.es>
2024-12-02 18:00:06 +0100

    Creacion de wiki sin errores

--- src/app/new-wiki/new-wiki.component.ts
+++ src/app/new-wiki/new-wiki.component.ts
@@ -1,34 +1,45 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { NewWikiService } from './new-wiki.service';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-wiki',
   standalone: true,
-  imports: [],
-  templateUrl: './new-wiki.component.html'
+  imports: [ReactiveFormsModule],
+  templateUrl: './new-wiki.component.html',
 })
-
 export class NewWikiComponent {
-  constructor(private router: Router, private wikisService: NewWikiService) {}
-  
+  wikiForm: FormGroup;
+
+  constructor(
+    private router: Router,
+    private wiki: NewWikiService,
+    private fb: FormBuilder
+  ) {
+    this.wikiForm = this.fb.group({
+      nombre: ['', Validators.required]
+    });
+  }
+
   volverAtras() {
-    console.log('Volver a la lista de wikis');
     this.router.navigate(['/']);
   }
-  wiki = {title: '', content: ''};
 
-  crearWiki(){
-    console.log('Crear una nueva wiki');
-    //Para crear una wiki tenemos que hacer una peticion a la base de datos
-    this.wikisService.createWiki(this.wiki).subscribe({
-      next: (response) => {
-        console.log("Wiki creada correctamente:", response);
-        //this.router.navigate(['/']);
-      },
-      error: (err) => {
-        console.error("Error al crear la wiki:", err);
-      },
-    });
+  crearWiki() {
+    if (this.wikiForm.valid) {
+      const wikiData = this.wikiForm.value;
+      this.wiki.createWiki(wikiData).subscribe({
+        next: (response) => {
+          console.log('wiki creada correctamente:', response);
+          this.router.navigate(['/']);
+        },
+        error: (err) => {
+          console.error('Error al crear la wiki:', err);
+        },
+      });
+    } else {
+      console.log('Formulario no válido');
+    }
   }
 }
