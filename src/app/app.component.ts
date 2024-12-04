84503cf644f614a64a9f13b716c29dd369dfa4e5
migueel15 <miguelangeldorado10@gmail.com>
2024-12-04 22:09:03 +0100

    autoScroll top on redirect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { NavigationEnd, Router, RouterOutlet } from "@angular/router";
 import { SubirImagenesComponent } from "./subir-imagenes/subir-imagenes.component";
-import { MapasComponent } from './mapas/mapas.component';
-import { WikisComponent } from './wikis/wikis.component';
-import { BarraNavegacionComponent } from './barra-navegacion/barra-navegacion.component';
-import { NotificacionesComponent } from './notificaciones/notificaciones.component';
+import { MapasComponent } from "./mapas/mapas.component";
+import { WikisComponent } from "./wikis/wikis.component";
+import { BarraNavegacionComponent } from "./barra-navegacion/barra-navegacion.component";
+import { NotificacionesComponent } from "./notificaciones/notificaciones.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [RouterOutlet, BarraNavegacionComponent, NotificacionesComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
-export class AppComponent {
-  title = 'laWiki';
+export class AppComponent implements OnInit {
+  title = "laWiki";
+  constructor(private router: Router) { }
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (!(event instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0);
+    });
+  }
 }
