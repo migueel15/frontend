93d273c9c7a075adb120e4e72c735a49f064db37
migueel15 <miguelangeldorado10@gmail.com>
2024-12-04 23:52:38 +0100

    comentarios reordenados

--- src/app/comentarios/comentarios.component.ts
+++ src/app/comentarios/comentarios.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ComentariosService } from './comentarios.service';
-import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
-
-
+import { Component } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { ComentariosService } from "./comentarios.service";
+import { CommonModule } from "@angular/common";
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-comentarios',
+  selector: "app-comentarios",
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
-  templateUrl: './comentarios.component.html'
+  templateUrl: "./comentarios.component.html",
 })
 export class ComentariosComponent {
   comentarioForm: FormGroup;
@@ -18,44 +21,45 @@ export class ComentariosComponent {
   comentarios: any[] = [];
 
   constructor(
-    private route : ActivatedRoute,
+    private route: ActivatedRoute,
     private comentariosService: ComentariosService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
   ) {
     this.comentarioForm = this.fb.group({
-      contenido: ['', Validators.required]
+      contenido: ["", Validators.required],
     });
   }
 
   ngOnInit(): void {
-    this.entradaId = this.route.snapshot.paramMap.get('id')!;
+    this.entradaId = this.route.snapshot.paramMap.get("id")!;
     this.comentariosService.getComentarios(this.entradaId).subscribe({
       next: (data) => {
         this.comentarios = data;
-        console.log('Comentarios:', this.comentarios);
+        console.log("Comentarios:", this.comentarios);
       },
       error: (err) => {
-        console.error('Error al obtener los comentarios:', err);
-      }
+        console.error("Error al obtener los comentarios:", err);
+      },
     });
   }
 
   crearComentario(): void {
-    if (this.comentarioForm.valid){
+    if (this.comentarioForm.valid) {
       const comentarioData = this.comentarioForm.value;
       comentarioData.idEntrada = this.entradaId;
       comentarioData.idUsuario = "64bda41e8e9f1a7a0e5c2e3f"; // ID de usuario fijo
-      
+
       this.comentariosService.crearComentario(comentarioData).subscribe({
         next: (response) => {
-          console.log('Comentario creado correctamente:', response);
-          // this.comentarios.push(comentarioData);
+          console.log("Comentario creado correctamente:", response);
+          // put the new comment at the top of the list
+          this.comentarios.unshift(response);
           this.comentarioForm.reset();
-          window.location.reload();
+          // window.location.reload();
         },
         error: (err) => {
-          console.error('Error al crear el comentario:', err);
-        }
+          console.error("Error al crear el comentario:", err);
+        },
       });
     }
   }
