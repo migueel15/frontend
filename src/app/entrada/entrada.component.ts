3475352858867ed7e5b7a7f6e813433f63e84b39
migueel15 <miguelangeldorado10@gmail.com>
2024-12-04 15:19:44 +0100

    añadir boton editar a entradas

--- src/app/entrada/entrada.component.ts
+++ src/app/entrada/entrada.component.ts
@@ -1,46 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { EntradaService } from './entrada.service';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { EntradaService } from "./entrada.service";
+import { CommonModule } from "@angular/common";
 import { VersionComponent } from "../version/version.component";
+import { BotonEditarComponent } from "../boton-editar/boton-editar.component";
 
 @Component({
-  selector: 'app-entrada',
+  selector: "app-entrada",
   standalone: true,
-  imports: [CommonModule, VersionComponent],
-  templateUrl: './entrada.component.html',
+  imports: [CommonModule, VersionComponent, BotonEditarComponent],
+  templateUrl: "./entrada.component.html",
 })
 export class EntradaComponent implements OnInit {
   entradaId!: string;
   versionActualId!: string;
-  nombreEntrada: string = '';
-  nombreUsuario: string = '';
+  nombreEntrada: string = "";
+  nombreUsuario: string = "";
   fechaCreacion: Date = new Date();
 
   constructor(
     private entradaService: EntradaService,
     private route: ActivatedRoute,
     private router: Router,
-  ) {}
+  ) { }
 
   ngOnInit(): void {
-    this.entradaId = this.route.snapshot.paramMap.get('id')!;
+    this.entradaId = this.route.snapshot.paramMap.get("id")!;
     this.entradaService.getEntradaById(this.entradaId).subscribe({
       next: (data) => {
-        this.versionActualId = data['idVersionActual']
-        this.nombreEntrada = data['nombre'];
-        this.nombreUsuario = data['nombreUsuario'];
-        this.fechaCreacion = new Date(data['fechaCreacion']);
+        this.versionActualId = data["idVersionActual"];
+        this.nombreEntrada = data["nombre"];
+        this.nombreUsuario = data["nombreUsuario"];
+        this.fechaCreacion = new Date(data["fechaCreacion"]);
       },
       error: (err) => {
-        console.error('Error al obtener la entrada:', err);
+        console.error("Error al obtener la entrada:", err);
       },
     });
   }
 
   verVersiones() {
-    console.log('Ver historial de versiones');
-
+    console.log("Ver historial de versiones");
     this.router.navigate([`/entrada/${this.entradaId}/versiones/`]);
+  }
+
+  editarEntrada() {
+    this.router.navigate([`/entrada/${this.entradaId}/editar`]);
   }
 }
